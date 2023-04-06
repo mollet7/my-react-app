1ae0057e6cc16c9fbdb8cba55b8bcf4a69571349
mollet7 <mollet7@live.com>
2023-04-06 15:56:19 +0300

    components

---
+++ src/Room.js
@@ -0,0 +1,4 @@
+function Room() {
+    return <h2> Hi, i'm a room inside the </h2>;
+}
+export default Room;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Room from './Room.js';
+
+
+
+
+
+
+
+const x = 9;
 
 const myelement = (
   <table>
@@ -15,3 +24,41 @@ const myelement = (
   </table>
 );
 ReactDOM.render(myelement, document.getElementById('root'));
+
+const sectionOne = (
+  <>
+    <p>I am a paragrapht</p>
+    <p>I am a paragraph too</p>
+    <p>The sum of 5 and 5 is {5 + 5}</p>
+  
+  <input type = "text" />
+  <p>{(x) < 10 ? "Hello" : "Goodbye"}</p>
+  </>
+);
+
+ReactDOM.render(sectionOne, document.getElementById('para1'));
+
+//React Component.
+//Functional Component.
+function Car(props){
+  return <h2>HI, I am a {props.color} Car!</h2>;
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById('comp1'));
+root.render(<Car color="red"/>);
+
+function Garage() {
+  return (
+    <>
+      <h1>Who lives in my Garage?</h1>
+      <Car />
+    </>
+  );
+}
+const root2 = ReactDOM.createRoot(document.getElementById('comp2'));
+root2.render(<Garage />);
+
+// Using component defined from file
+const root3 = ReactDOM.createRoot(document.getElementById('compFromFile'));
+root3.render(<Room />);
